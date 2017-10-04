f8038a989bc8278120972c66469e50f1dfcda37f
Ivan Mattie <mattieivan@gmail.com>
2017-10-03 20:10:07 -0600

    Set up the code to be ready to be a CLI app

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+#! /usr/bin/env node
+
+const args = require("really-simple-args")();
 const LighthouseCrawler = require("./core/LighthouseCrawler");
+
+if(args.getAmountOfArguments() < 1) {
+    console.log("No URL specified.");
+
+    process.exit(1);
+}
+
+const startURL = args.getArgumentByIndex(0);
 
 let crawler = new LighthouseCrawler();
 
-crawler.crawl("https://www.domainmarket.com");
+crawler.crawl(startURL);
