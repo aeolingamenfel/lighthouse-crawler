d2e8a96fc5e7390f3e89540495db883a49cb5cb1
Ivan Mattie <mattieivan@gmail.com>
2017-10-03 20:00:29 -0600

    Added basic docblocks

--- core/LighthouseCrawler.js
+++ core/LighthouseCrawler.js
@@ -5,8 +5,16 @@ const Crawler = require("node-webcrawler");
 
 const SitePage = require("./SitePage.js");
 
+/**
+ * Class representing and controlling the site crawl.
+ */
 class LighthouseCrawler {
 
+    /**
+     * 
+     * @param {Integer=} crawlLimit the amount of pages on the site to crawl, 
+     *  defaults to 10.
+     */
     constructor(crawlLimit = 10) {
         this.crawler = null;
         this.linksQueued = false;
@@ -15,6 +23,11 @@ class LighthouseCrawler {
         this.crawlLimit = crawlLimit;
     }
 
+    /**
+     * Begins the crawl starting at the URL specified.
+     * 
+     * @param {String} startUrl A valid URL to start the crawl at.
+     */
     crawl(startUrl) {
         this.crawler = new Crawler({
             maxConnections: 2,
@@ -31,6 +44,16 @@ class LighthouseCrawler {
         console.log("Beginning site crawl...");
     }
 
+    /**
+     * Called when a particular page is loaded by the crawler.
+     * 
+     * @param {Boolean} error True if there was an error attempting to crawl 
+     *  the page.
+     * @param {Object} result The result of the crawl, including the request and 
+     *  other information.
+     * @param {Object} $ a lightweight, server-side jQuery implementation for 
+     *  interfacing with the webpage.
+     */
     crawlLoadedPage(error, result, $) {
         this.pageWasFound(result.request);
 
@@ -39,12 +62,24 @@ class LighthouseCrawler {
         this.queueLinks(links);
     }
 
+    /**
+     * Called when a page was found, loaded and confirmed.
+     * 
+     * @param {Request} request The request associated with the page load.
+     */
     pageWasFound(request) {
         const url = request.uri.href;
         
         this.registerPage(url);
     }
 
+    /**
+     * Takes an array of URLs and will queue them for the crawler, assuming
+     * that we haven't reached the limit.
+     * 
+     * @param {Array<String>} links A list of links to be queued for the 
+     *  crawler. 
+     */
     queueLinks(links) {
         if(this.crawledCount >= this.crawlLimit) {
             return;
@@ -58,6 +93,12 @@ class LighthouseCrawler {
         }
     }
 
+    /**
+     * Attempts to queue a particular URL to be crawled, assuming it hasn't 
+     * already been queued/added.
+     * 
+     * @param {String} url A page URL to be added.
+     */
     addCrawledPage(url) {
         if(this.crawledCount >= this.crawlLimit) {
             return;
@@ -72,12 +113,28 @@ class LighthouseCrawler {
         }
     }
 
+    /**
+     * Checks if a particular URL has already be crawled.
+     * 
+     * @param {String} url The URL to check.
+     * 
+     * @returns {Boolean} true if the URL has already been crawled, or false 
+     *  otherwise.
+     */
     hasPage(url) {
         const truncatedUrl = this.truncateUrl(url);
 
         return !!this.crawledPages[truncatedUrl];
     }
 
+    /**
+     * Attempts to register a particular page via it's URL.
+     * 
+     * @param {String} url The URL to register and check.
+     * 
+     * @returns {Boolean|SitePage} Either false if the page was already 
+     *  registered, or the SitePage object that was added otherwise.
+     */
     registerPage(url) {
         if(this.hasPage(url)) {
             return false;
@@ -95,10 +152,26 @@ class LighthouseCrawler {
         return page;
     }
 
+    /**
+     * Checks if a URL is bad, which is any URL that can't easily be loaded, 
+     * or is just generally not a URL to a page.
+     * 
+     * @param {String} url The URL to check whether it is bad or not.
+     * 
+     * @returns {Boolean} True if the URL is bad, or false otherwise.
+     */
     isBadUrl(url) {
         return /^(?:(?:(?:mailto)|(?:tel))[:])|(#)/.test(url);
     }
 
+    /**
+     * Removes GET variables, anchor links, protocol, and other information 
+     * from the base URL, so that it can be easily compared with other URLs.
+     * 
+     * @param {String} url The raw URL to truncate.
+     * 
+     * @returns {String} The truncated version of the URL.
+     */
     truncateUrl(url) {
         const shortened = url.replace(/^http[s]?\:\/\//i, "")
             .replace(/[\/]?(?:#(?:[a-z]|[A-Z]|[0-9]|[-])*)*(?:\?.*)*$/i, "");
@@ -106,12 +179,21 @@ class LighthouseCrawler {
         return shortened;
     }
 
+    /**
+     * Called automatically when the crawler doesn't have anything remaining 
+     * in its pool of URLs. This is a good place to initiate page tests.
+     * 
+     * @param {Object} pool 
+     */
     drainHasBegin(pool) {
         console.log(`Crawl Complete. ${this.crawledCount} page(s) crawled.`);
 
         this.testAllPages();
     }
 
+    /**
+     * Runs a lighthouse test on all crawled pages.
+     */
     async testAllPages() {
         console.log("Preparing to run tests on all crawled pages. Do not close or tamper with the Chrome window that will appear.");
 
@@ -136,6 +218,11 @@ class LighthouseCrawler {
         fs.unlinkSync("./temp.json");
     }
 
+    /**
+     * Runs a Lighthouse test on an individual page.
+     * 
+     * @param {SitePage} page The page to be tested.
+     */
     async testPage(page) {
         const score = await this.scorePage(page);
 
@@ -144,6 +231,12 @@ class LighthouseCrawler {
         console.log(`${page.url}: ${page.loadScore}`);
     }
 
+    /**
+     * Initiates and gathers the information from a lighthouse test on the 
+     * specified page.
+     * 
+     * @param {SitePage} page
+     */
     async scorePage(page) {
         const command = `lighthouse ${page.fullUrl} --output json --output-path ./temp.json`;
 
--- core/SitePage.js
+++ core/SitePage.js
@@ -1,5 +1,13 @@
+/**
+ * Wrapper class representing a page to be tested.
+ */
 class SitePage {
 
+    /**
+     * 
+     * @param {String} url The truncated URL representing this page. 
+     * @param {String} fullUrl The raw, un-truncated URL representing this page.
+     */
     constructor(url, fullUrl) {
         this.url = url;
         this.fullUrl = fullUrl;
@@ -8,6 +16,12 @@ class SitePage {
         this.loadScore = -1;
     }
 
+    /**
+     * Called by the overlying LighthouseCrawler when it finishes the test on 
+     * this page, with the score information data.
+     * 
+     * @param {Object} score
+     */
     didRecieveScore(score) {
         this.completed = true;
         this.score = score;
@@ -15,6 +29,10 @@ class SitePage {
         this.generateLoadScore();
     }
 
+    /**
+     * Finds and stores the relevant score for this page from the raw score data
+     * from Lighthouse.
+     */
     generateLoadScore() {
         for(let x = 0; x < this.score.reportCategories.length; x++) {
             const category = this.score.reportCategories[x];
